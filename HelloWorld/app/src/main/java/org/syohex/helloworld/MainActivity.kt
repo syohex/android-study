3bf014cf4955d1a43eae3e91c69b14dd3f32a042
Shohei YOSHIDA <syohex@gmail.com>
2021-04-15 01:04:26 +0900

    Add counter button

--- HelloWorld/app/src/main/java/org/syohex/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/org/syohex/helloworld/MainActivity.kt
@@ -1,11 +1,23 @@
 package org.syohex.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClickMe = findViewById<Button>(R.id.button)
+        val myTextView = findViewById<TextView>(R.id.textView)
+        var timeClicked = 0
+        buttonClickMe.setOnClickListener {
+            timeClicked += 1
+            myTextView.text = timeClicked.toString()
+            Toast.makeText(this@MainActivity, "Hi Bomber", Toast.LENGTH_LONG).show()
+        }
     }
 }
