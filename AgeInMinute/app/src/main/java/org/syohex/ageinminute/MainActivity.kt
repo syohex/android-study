0f8c81873423dc0c81e70a3a0c985e52180d9bdd
Shohei YOSHIDA <syohex@gmail.com>
2021-04-26 22:46:17 +0900

    Add date picker

--- AgeInMinute/app/src/main/java/org/syohex/ageinminute/MainActivity.kt
+++ AgeInMinute/app/src/main/java/org/syohex/ageinminute/MainActivity.kt
@@ -1,11 +1,31 @@
 package org.syohex.ageinminute
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonDatePicker.setOnClickListener { view ->
+            clickDatePicker(view)
+        }
+    }
+
+    private fun clickDatePicker(view: View) {
+        val myCalendar = Calendar.getInstance();
+        val year = myCalendar.get(Calendar.YEAR);
+        val month = myCalendar.get(Calendar.MONTH);
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog(this, { view, year, month, dayOfMonth ->
+            Toast.makeText(this, "DatePicker works", Toast.LENGTH_LONG).show()
+        }, year, month, day).show()
     }
 }
